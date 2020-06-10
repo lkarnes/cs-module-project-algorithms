c62ed241198aae1d48253d8bd0aba97344375b8c
Logan Karnes <logan.karnes98@gmail.com>
2020-06-09 21:41:55 -0700

    finished the product of all numbers

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -3,14 +3,19 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def product_of_all_other_numbers(arr):
-    # Your code here
-
-    pass
-
-
+    new_arr = list(arr)
+    for i in range(len(arr)):
+        item = new_arr.pop(i)
+        total = 1
+        for j in new_arr:
+            total *= j
+        arr[i] = total
+        new_arr.insert(i, item)
+        total = 1
+    return arr
 if __name__ == '__main__':
     # Use the main function to test your implementation
     # arr = [1, 2, 3, 4, 5]
-    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    arr = [1, 7, 3, 4]
 
     print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
